d315ba454ee16233a4c8a5783dadf0f930ebd946
Jamie Matthews <jamie.matthews@gmail.com>
2010-10-03 00:28:03 +0100

    I can haz comments?

--- gitstat.php
+++ gitstat.php
@@ -1,33 +1,53 @@
 <?php
 
+/**
+ * Class to represent a Git repository
+ *
+ * @author Jamie Matthews 2010-10-03
+ */
 class RepoChecker {
 
     protected $_flags;
     protected $_git_command = 'git';
 
+    /**
+     * Construct a new RepoChecker for the given repository
+     */
     public function __construct($repo_dir) {
         $repo_dir = realpath($repo_dir);
         $this->_flags = "--git-dir={$repo_dir}/.git --work-tree={$repo_dir}";
     }
 
+    /**
+     * Is the working tree for this repository dirty?
+     */
     public function is_dirty() {
         $command = "{$this->_git_command} {$this->_flags} status --porcelain";
         $output = trim(shell_exec($command));
         return $output !== '';
     }
 
+    /**
+     * Return the URL for the specified remote
+     */
     public function get_remote_url($remote_name) {
         $command = "{$this->_git_command} {$this->_flags} config --get remote.{$remote_name}.url";
         $output = trim(shell_exec($command));
         return $output;
     }
 
+    /**
+     * Is this repository out of sync with the specified remote?
+     */
     public function is_out_of_sync_with($remote_name, $remote_ref='HEAD') {
         $remote_sha1 = $this->_get_remote_sha1($remote_name, $remote_ref);
         $local_sha1 = $this->_get_local_sha1();
         return $remote_sha1 !== $local_sha1;
     }
 
+    /**
+     * Get the SHA1 of the given remote and ref (ref defaults to HEAD)
+     */
     protected function _get_remote_sha1($remote_name, $remote_ref='HEAD') {
         $command = "{$this->_git_command} {$this->_flags} ls-remote {$remote_name} {$remote_ref}";
 
@@ -37,6 +57,9 @@ class RepoChecker {
         return $remote_sha1;
     }
 
+    /**
+     * Get the SHA1 of the HEAD of the given local branch (default MASTER)
+     */
     protected function _get_local_sha1($branch='MASTER') {
         $command = "{$this->_git_command} {$this->_flags} rev-list --max-count=1 {$branch} HEAD";
         $output = trim(shell_exec($command));
@@ -45,8 +68,10 @@ class RepoChecker {
 
 }
 
+// Parse the configuration INI file
 $config = parse_ini_file('config.ini', true);
 
+// Retrieve general configuration data
 $general_config = $config['General'];
 unset($config['General']);
 
@@ -56,11 +81,13 @@ $repos = $config;
 // Set timezone
 date_default_timezone_set('Europe/London');
 
+// Set up the output data array
 $output_data = array(
     'last_updated' => date('H:i, l jS F Y'),
     'repositories' => array(),
 );
 
+// Add the details for each repository to the output data array
 foreach ($repos as $name => $repo) {
     $checker = new RepoChecker($repo['path']);
     $repo_data = array(
@@ -74,5 +101,6 @@ foreach ($repos as $name => $repo) {
     $output_data['repositories'][] = $repo_data;
 }
 
+// Dump the repository data as JSON into the file set in the config
 file_put_contents($general_config['output_file'], json_encode($output_data), LOCK_EX);
 
